c6144165d9d6606530a4b1d948af8b9b5a99b2e1
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-01-03 17:46:19 -0600

    Added bingo generating code

--- public/index.js
+++ public/index.js
@@ -4,6 +4,7 @@ function onLoad() {
     document.getElementById("generateButton").onclick = (ev) => {
         const input = document.getElementById("optionsInput").value;
         const bingoCard = generateBingoObject(input);
+        console.log(bingoCard);
     }
     document.getElementById("optionsInput").oninput = (ev) => {
         setErrorMessage("");
@@ -18,8 +19,25 @@ function generateBingoObject(inputText) {
         setErrorMessage(`Must provide at least ${SIZE*SIZE} options; only ${lines.length} given.`);
         return;
     }
+    // Start grabbing random items from the lines, adding it to the 2D array, and then removing it from lines
+    let bingoCard = [];
+    let currentRow = [];
+    for (let i = 0; i < SIZE; i++) {
+        currentRow = [];
+        for (let j = 0; j < SIZE; j++) {
+            let item = popRandomItem(lines);
+            currentRow.push(item);
+        }
+        bingoCard.push(currentRow);
+    }
+    return bingoCard;
 }
 
 function setErrorMessage(msg) {
     document.getElementById("errorText").innerText = msg;
+}
+
+function popRandomItem(list) {
+    const removedElems = list.splice(Math.floor(Math.random()*list.length), 1);
+    return removedElems[0];
 }
